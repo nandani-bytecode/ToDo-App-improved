24928fa6b5afc0e2b23385ea6ef811c8a394751c
nandani-bytecode <nandanikandoi@gmail.com>
2026-06-18 17:23:58 +0530

    version 1 of to do app

---
+++ src/components/Todoitem1.jsx
@@ -0,0 +1,20 @@
+function Todoitem1(){
+    return (
+            <div class="container text-center">
+  
+
+  <div class="row kg-row">
+    <div class="col-6">
+      go to college
+    </div>
+    <div class="col-4">
+      4/10/2023
+    </div>
+    <div class="col-2">
+      <button type="button" class="btn btn-danger kg-button">delete</button>
+    </div>
+  </div>
+  </div>
+    )
+}
+export default Todoitem1;
